c301f92f72b64b6e242ac82541e2ea56ad091b56
Anas Misbah <anas.misbahuddin4869@gmail.com>
2023-03-13 22:31:43 +0800

    update intro view

--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -18,8 +18,9 @@ class IntroductionView extends GetView<IntroductionController> {
             Text(
               "Al-quran Apps",
               style: TextStyle(
-                fontSize: 25,
+                fontSize: 28,
                 fontWeight: FontWeight.bold,
+                color: Color(0xFF672CBC),
               ),
             ),
             SizedBox(
@@ -28,29 +29,63 @@ class IntroductionView extends GetView<IntroductionController> {
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 40),
               child: Text(
-                "Sesibuk itukah kamu sampai belum membaca alquran ?",
+                "Learn Quran and Recite once everyday",
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                  fontSize: 16,
+                  fontSize: 18,
+                  color: Color(0xFF8789A3),
                 ),
               ),
             ),
             SizedBox(
-              height: 20,
+              height: 49,
             ),
-            Container(
-              width: 250,
-              height: 250,
-              child: Lottie.asset("assets/lotties/animasi-quran.json"),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Get.offAllNamed(Routes.HOME);
-              },
-              child: Text("MULAI"),
+            Stack(
+              clipBehavior: Clip.hardEdge,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(horizontal: 30),
+                      padding: EdgeInsets.only(bottom: 80),
+                      alignment: Alignment.topCenter,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        color: Color(0xFF3B1E77),
+                      ),
+                      child: Image.asset(
+                        'assets/images/introduction.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                  ],
+                ),
+                Positioned.fill(
+                  child: Align(
+                    alignment: AlignmentDirectional.bottomCenter,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.offAllNamed(Routes.HOME);
+                      },
+                      child: Text("Get Started"),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFF9B091),
+                          padding: EdgeInsets.symmetric(
+                              vertical: 16, horizontal: 30),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          textStyle: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          )),
+                    ),
+                  ),
+                )
+              ],
             )
           ],
         ),
